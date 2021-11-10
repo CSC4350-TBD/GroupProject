9488cb3ffc6df11576904f1b5a2d8c11ca3f2929
AlexOrtiz42 <ortizmail42@gmail.com>
2021-11-10 02:32:47 +0000

    Check for conflicts

--- recommend.py
+++ recommend.py
@@ -1,4 +1,5 @@
-from moviedb import get_movie_info, get_id
+from GroupProject.model import genre_exclusions
+from moviedb import get_movie_info, get_id, get_movie_recs
 from imdb import get_imdb_id
 
 def get_recommendation(): #this is where you will pass the entered movie.
@@ -8,6 +9,9 @@ def get_recommendation(): #this is where you will pass the entered movie.
     imdbid, imdb_api_img = get_imdb_id(search_term) #get usable id from imdb/also a poster
     moviedb_id = get_id(imdbid)                     #get moviedb id from imdbid
     movie_genre, movie_title = get_movie_info(moviedb_id)   #get details about the movie
+    
+    selected_genre = movie_genre #can change this when we allow for just a search based on genre
+    genre_exclusion = ""        #this will need to be a db call. (SELECT genre FROM genre_exclusions WHERE username = currentuser.username)
 
 
 
