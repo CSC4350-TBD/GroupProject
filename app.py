9f08e075ed23c6e1ca51d7e8f411af16d8c27f50
sayu77 <syu18@student.gsu.edu>
2021-11-11 03:26:30 +0000

    login required uncommented

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ db_url = os.getenv("DATABASE_URL")
 if db_url.startswith("postgres://"):
     db_url = db_url.replace("postgres://", "postgresql://", 1)
 app.config["SQLALCHEMY_DATABASE_URL"] = db_url
-app.config["SQLALCHEMY_DATABASE_URI"] = db_url
+# app.config["SQLALCHEMY_DATABASE_URI"] = db_url
 
 # Gets rid of a warning
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
